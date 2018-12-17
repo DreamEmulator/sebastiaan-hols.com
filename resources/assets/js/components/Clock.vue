177700786567ebbe16743aff3a24f341f8ef18c8
Sebastiaan Hols <shg.hols@gmail.com>
2018-12-17 20:27:57 +0100

    Added Nomos Metro Gangreserve - Analogue/Digital Clock

--- resources/assets/js/components/Clock.vue
+++ resources/assets/js/components/Clock.vue
@@ -8,7 +8,7 @@
                     <div class="card-body clock-container">
 
                         <div id="clock">
-                            <div id="gangreserve-backdial" v-bind:style="{transform: gangreserve_rotation}">
+                            <div id="gangreserve-backdial" v-bind:style="{transform: gangreserve_backdial_rotation}">
                                 <div id="gangreserve" v-bind:style="{transform: gangreserve_rotation}"></div>
                             </div>
                             <div v-if="hacking_seconds" v-on:click="hackSeconds()" id="crown-in"></div>
@@ -19,14 +19,19 @@
                                 <div id="second" v-bind:style="{transform: second_rotation}"></div>
                                 <div class="base"></div>
                             </div>
-                            <div id="minute" class="load-transition" v-bind:style="{transform: minute_rotation}"></div>
-                            <div id="hour" class="load-transition" v-bind:style="{transform: hour_rotation}"></div>
+                            <div id="minute" v-bind:style="{transform: minute_rotation}"></div>
+                            <div id="hour" v-bind:style="{transform: hour_rotation}"></div>
                             <div class="base"></div>
                         </div>
                     </div>
                     <div class="card-footer text-muted">
-                        <button v-if="!shanghai_time" v-on:click="setShanghaiTime()"v-bind:class="{'text-muted': !hacking_seconds}" class="w-100 btn btn-light">Shanghai time</button>
-                        <button v-if="shanghai_time" v-on:click="setShanghaiTime()"v-bind:class="{'text-muted': !hacking_seconds}" class="w-100 btn btn-light">Local time</button>
+                        <button v-if="!shanghai_time" v-on:click="setShanghaiTime()"
+                                v-bind:class="{'text-muted': !hacking_seconds}" class="w-100 btn btn-light">Shanghai
+                            time
+                        </button>
+                        <button v-if="shanghai_time" v-on:click="setShanghaiTime()"
+                                v-bind:class="{'text-muted': !hacking_seconds}" class="w-100 btn btn-light">Local time
+                        </button>
                     </div>
                 </div>
             </div>
@@ -47,33 +52,34 @@
                 hour: 0,
                 hour_rotation: '',
                 day: 0,
-                power: 100,
+                power: 0,
                 interval: null,
                 date: null,
                 hacking_seconds: false,
                 gangreserve_degrees: 60,
                 gangreserve_rotation: '',
+                gangreserve_backdial_rotation: '',
                 shanghai_time: false,
-                transition_timeout: null,
                 GMT: null,
+                last_visit: null,
             }
         },
         methods: {
-            hackSeconds: function (){
+            hackSeconds: function () {
                 this.hacking_seconds = !this.hacking_seconds;
                 clearInterval(this.interval);
             },
             startClock: function () {
                 var time = new Date;
-                this.GMT = time.getTimezoneOffset()/60;
-                if (!this.hacking_seconds){
+                this.GMT = time.getTimezoneOffset() / 60;
+                if (!this.hacking_seconds) {
                     this.hacking_seconds = !this.hacking_seconds;
                     this.interval = setInterval(() => {
                         this.setTime()
                     }, 1000 / 6);
                 }
             },
-            setShanghaiTime: function(){
+            setShanghaiTime: function () {
                 clearInterval(this.transition_timeout);
                 this.shanghai_time = !this.shanghai_time;
             },
@@ -83,41 +89,73 @@
                 this.second = this.date.getSeconds();
                 this.millisecond = this.date.getMilliseconds();
                 this.second_rotation = "rotate(" + ((this.second + (this.millisecond / 1000)) * 6 - 90) + "deg) translateY(-50%)";
-                this.second.toString().length === 1 ? this.second = '0' + this.second :null;
 
                 this.minute = this.date.getMinutes();
-                this.minute_rotation = "rotate(" + ((this.minute + ((this.second + (this.millisecond / 1000)) / 60)) * 6 - 90)+ "deg) translateY(-50%)";
-                this.minute.toString().length === 1 ? this.minute = '0' + this.minute :null;
+                this.minute_rotation = "rotate(" + ((this.minute + ((this.second + (this.millisecond / 1000)) / 60)) * 6 - 90) + "deg) translateY(-50%)";
 
                 this.hour = this.date.getHours();
                 this.shanghai_time ? this.hour = this.hour + (8 + this.GMT) : null;
                 this.hour > 24 ? this.hour = this.hour - 24 : null;
-                this.hour_rotation = "rotate(" + (((this.hour + ((this.minute + ((this.second + (this.millisecond / 1000)) / 60)) / 60))* 30) - 90)  + "deg) translateY(-50%)";
-                this.hour.toString().length === 1 ? this.hour = '0' + this.hour :null;
+                this.hour_rotation = "rotate(" + (((this.hour + ((this.minute + ((this.second + (this.millisecond / 1000)) / 60)) / 60)) * 30) - 90) + "deg) translateY(-50%)";
 
-                this.gangreserve_degrees = this.gangreserve_degrees + (360/(42*60*60*60*6));
+                this.gangreserve_degrees = this.gangreserve_degrees + (360 / (42 * 60 * 60 * 60 * 6));
                 this.gangreserve_rotation = "rotate(" + this.gangreserve_degrees + "deg)";
+                this.gangreserve_backdial_rotation = "rotate(-" + this.gangreserve_degrees + "deg)";
 
                 this.day = this.date.getDate();
+
+                this.second.toString().length === 1 ? this.second = '0' + this.second : null;
+                this.minute.toString().length === 1 ? this.minute = '0' + this.minute : null;
+                this.hour.toString().length === 1 ? this.hour = '0' + this.hour : null;
             },
             setSize: function () {
                 this.clock_diameter = $("#clock").width();
                 $("#clock").height(this.clock_diameter);
                 $("#clock .base").css({
-                    "width":this.clock_diameter*0.05 + "px",
-                    "height":this.clock_diameter*0.05 + "px",
-                    "border-width":this.clock_diameter*0.015 + "px",
+                    "width": this.clock_diameter * 0.05 + "px",
+                    "height": this.clock_diameter * 0.05 + "px",
+                    "border-width": this.clock_diameter * 0.015 + "px",
                 });
                 $("#clock .second-container .base").css({
-                    "width":this.clock_diameter*0.015 + "px",
-                    "height":this.clock_diameter*0.015 + "px",
-                    "border-width":this.clock_diameter*0.005 + "px",
+                    "width": this.clock_diameter * 0.015 + "px",
+                    "height": this.clock_diameter * 0.015 + "px",
+                    "border-width": this.clock_diameter * 0.005 + "px",
                 })
+            },
+            setCookie: function (cname, cvalue, exdays) {
+                var d = new Date();
+                d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+                var expires = "expires=" + d.toUTCString();
+                document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+            },
+            getCookie: function (cname) {
+                var name = cname + "=";
+                var decodedCookie = decodeURIComponent(document.cookie);
+                var ca = decodedCookie.split(';');
+                for (var i = 0; i < ca.length; i++) {
+                    var c = ca[i];
+                    while (c.charAt(0) == ' ') {
+                        c = c.substring(1);
+                    }
+                    if (c.indexOf(name) == 0) {
+                        return c.substring(name.length, c.length);
+                    }
+                }
+                return "";
+            },
+            getLastVisit: function(){
+                this.last_visit = this.getCookie('clock');
+            },
+            setVisit: function () {
+                var cookie_time = Math.round((new Date()).getTime() / 1000);
+                this.setCookie('clock',cookie_time,30);
             }
         },
         created: function () {
             console.log("Vue ready");
             this.startClock();
+            this.getLastVisit();
+            this.setVisit();
             $('document').ready(() => {
                 this.setSize()
             });
@@ -220,11 +258,13 @@
         position: absolute;
         top: 50%;
         left: 50%;
-        width: 39.75%;
+        width: 39.25%;
         height: 1%;
         background-color: #0d0d0d;
         transform-origin: 0 0;
         border-radius: 0.2vmin;
+        transition: 1s;
+        transition-timing-function: ease-out;
     }
 
     #minute:before {
@@ -244,11 +284,13 @@
         position: absolute;
         top: 50%;
         left: 50%;
-        width: 28%;
+        width: 29%;
         height: 1%;
         background-color: #0d0d0d;
         transform-origin: 0 0;
         border-radius: 0.2vmin;
+        transition: 2s;
+        transition-timing-function: ease-out;
     }
 
     #hour:before {
@@ -264,7 +306,7 @@
         background-color: #0d0d0d;
     }
 
-    #clock .base{
+    #clock .base {
         position: absolute;
         background-color: #c6c6c6;
         border-radius: 100%;
@@ -315,7 +357,7 @@
         position: absolute;
         bottom: 5%;
         left: 50%;
-        transform: translateX(-50%) scale(1.5,2.5);
+        transform: translateX(-50%) scale(1.5, 2.5);
         color: #111;
         padding: 0.5vmax 0.75vmax;
         font-size: 1.25vmax;
@@ -333,8 +375,4 @@
         width: 5vmax;
     }
 
-    .load-transition {
-        transition: 1s;
-        transition-timing-function: ease-out;
-    }
 </style>
