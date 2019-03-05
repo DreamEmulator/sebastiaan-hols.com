80c89e93a0943e6c1a24b8e60a4f47fcdef4f014
Sebastiaan Hols <shg.hols@gmail.com>
2019-03-05 07:55:38 +0100

    Fixed vue laravel mix with adding .default op requires

--- resources/assets/js/components/VueLoader.vue
+++ resources/assets/js/components/VueLoader.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="loading-target">
         <vue-magnifier ref="magnifier" v-if="index < load" v-for="(painting,index) in paintings" v-bind:src="painting.location" v-bind:src-large="painting.location" :painter="painting.subtitle" :title="painting.title" :key="painting.id" :index="painting.id"></vue-magnifier>
-        <button v-if="load !== paintings.length" class="mt-2 w-25 btn rainborder" v-on:click="load_extra">More art!</button>
+        <button v-if="load !== paintings.length" class="mt-2 p-4 btn rainborder" v-on:click="load_extra">More art!</button>
     </div>
 </template>
 
