46b86c78490a15c044008aec20c808a463ef2d99
Sebastiaan Hols <s.hols@icloud.com>
2020-12-27 16:48:58 +0100

    Added play / pause functionality to the gallery carousel

--- resources/assets/js/components/PhotoLibrary.vue
+++ resources/assets/js/components/PhotoLibrary.vue
@@ -1,38 +1,70 @@
 <template>
-    <div class="row">
-        <gallery :images="locations" :index="index" @close="index = null"></gallery>
+  <div class="row">
+    <gallery :images="locations" :index="index" @close="index = null; galleryOpen = false" ref="blueImpGallery"></gallery>
 
-        <div class="card-columns col-lg-12">
-            <div v-for="(image, imageIndex) in image_data" class="card rounded-card d-inline-block mt-2 mb-2 w-100">
-                <img class="card-img-top"
-                     loading="lazy"
-                     @click="index = imageIndex"
-                     :src="image.location"
-                >
-                <div class="card-body">
-                    <h4 class="card-title">{{image.title}}</h4>
-                    <p class="card-text">{{image.text}}</p>
-                </div>
-            </div>
+    <div class="card-columns col-lg-12">
+      <div v-for="(image, imageIndex) in image_data" class="card rounded-card d-inline-block mt-2 mb-2 w-100">
+        <img class="card-img-top"
+             loading="lazy"
+             @click="index = imageIndex; galleryOpen = true"
+             :src="image.location"
+        >
+        <div class="card-body">
+          <h4 class="card-title">{{ image.title }}</h4>
+          <p class="card-text">{{ image.text }}</p>
         </div>
-
+      </div>
     </div>
+    <button id="galleryPlayButton" v-if="galleryOpen" v-on:click="playGallery">{{ !this.galleryPlaying ? 'Play' : 'Pause' }}</button>
+  </div>
 </template>
 
 <script>
-    import VueGallery from 'vue-gallery';
+import VueGallery from 'vue-gallery';
 
-    export default {
-        props: ['photos'],
-        data: function () {
-            return {
-                image_data: this.photos,
-                locations: _.map(this.photos, 'location'),
-                index: null
-            };
-        },
-        components: {
-            'gallery': VueGallery
-        },
+export default {
+  props: ['photos'],
+  data: function () {
+    return {
+      image_data: this.photos,
+      locations: _.map(this.photos, 'location'),
+      index: null,
+      galleryOpen: false,
+      galleryPlaying: false,
+    };
+  },
+  components: {
+    'gallery': VueGallery
+  },
+  methods:{
+    playGallery: function (){
+      if(!this.galleryPlaying){
+        this.galleryPlaying = true
+        this.$refs['blueImpGallery'].instance.play();
+      } else {
+        this.galleryPlaying = false
+        this.$refs['blueImpGallery'].instance.pause();
+      }
     }
+  }
+}
 </script>
+
+<style>
+#galleryPlayButton {
+  position: fixed;
+  bottom: 1vh;
+  left: 1vh;
+  z-index: 1000000;
+  color: rgba(255, 255, 255, 0.5);
+  background: transparent;
+}
+
+#blueimp-gallery .next, #blueimp-gallery .prev {
+  opacity: 0.1;
+}
+
+#blueimp-gallery .next:hover, #blueimp-gallery .prev:hover {
+  opacity: 0.5;
+}
+</style>
